2c795a9d2d3a3f577a7cbda3953959bb0bda33d8
Zac Smith <zrs3141592@gmail.com>
2020-04-03 00:09:23 -0600

    add basic react / redux app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Routes from './routes';
+
+
+//main app 
+class App extends Component {
+
+  render() {
+    return(
+      <div>
+      React
+      <Routes />
+      </div>
+    )}
+}
+
+
+export default App;
