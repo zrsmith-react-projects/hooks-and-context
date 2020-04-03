701b2f5d6869f8f31cdf8ba5b6ec8868168dc9bc
Zac Smith <zrs3141592@gmail.com>
2020-04-03 00:45:03 -0600

    useEffect

--- src/hooks/hooks_container1.js
+++ src/hooks/hooks_container1.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 const HooksContainer1 = () => {
 
   const [stateValue, setValue] = useState(0)
+  const [useEffectValue, setUseEffectValue] = useState(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setUseEffectValue('time out worked')
+    }, 5000)
+  }, [stateValue])
 
   const increment = () => {
     setValue(stateValue + 1)
@@ -11,12 +18,26 @@ const HooksContainer1 = () => {
     setValue(stateValue - 1)
   }
 
+  const changeUseEffectValue = () => {
+    setUseEffectValue('some string')
+  }
+
   return (
     <div>
       <p>hooks container 1</p>
-      <p>Local State: {stateValue}</p>
+
       <button onClick={() => increment()}>+1 local state</button>
       <button onClick={() => decrement()}>-1 local state</button>
+      <button onClick={() => changeUseEffectValue()}>Change useEffect</button>
+      <div>
+        <p>
+          {useEffectValue
+            ? <p>{useEffectValue}</p>
+            : <p>No value</p>
+          }
+        </p>
+        <p>Local State: {stateValue}</p>
+      </div>
     </div>)
 };
 
